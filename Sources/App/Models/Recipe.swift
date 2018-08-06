aaa840ac4e2c04b373ed79b4afe7ade8cbdfe040
Andrew Madsen <andrew@openreelsoftware.com>
2018-08-05 20:51:01 -0600

    Add /recipes endpoint

---
+++ Sources/App/Models/Recipe.swift
@@ -0,0 +1,15 @@
+//
+//  Recipe.swift
+//  App
+//
+//  Created by Andrew R Madsen on 8/5/18.
+//
+
+import Vapor
+
+struct Recipe: Content {
+    
+    var name: String
+    var instructions: String
+    
+}
